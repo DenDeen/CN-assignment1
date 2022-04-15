017640a2ecb960263dc960845f3e48ea678a88c3
brian.verbanck <brian.verbanck@student.howest.be>
2022-04-15 16:45:58 +0200

    added basic http responses

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
+from base64 import encode
+from http.client import OK
 import socket
 from os.path import exists
+from datetime import date
+
+from client import getContentLength
 
 def getRequest(connection, uri):
     print("connection: " + connection + "  uri: " + uri)
@@ -10,23 +15,49 @@ def headRequest(connection, uri):
 
 
 def postRequest(connection, uri, data):
-    if exists(uri):
+    if exists(uri + '.txt'):
         file = open(uri + '.txt', 'a')
         file.write(data)
-        connection.send('200 Ok'.encode())
+        connection.send(Ok200())
     else:
-        connection.send('404 Not Found'.encode())
+        connection.send(BadRequest400())
    
     connection.close()
 
 
 def putRequest(connection, uri, data):
     print("connection: " + str(connection) + "  uri: " + uri + " data: " + data)
-    with open(uri + '.html','w') as f:
+    with open(uri + '.txt','w') as f:
             f.write(str(data))
     
-    connection.send('200 Ok'.encode())
+    connection.send(Ok200())
     connection.close()
+
+def Ok200():
+    return ("200 Ok \n" + getDate() + "\n" + getContentType + "\n" + getContentLength).encode()
+
+def NotModified304():
+    return ("304 Not Modified \n" + getDate()).encode()
+
+def BadRequest400():    
+    return ("400 Bad Request \n" + getDate()).encode()
+
+def NotFound404():
+    return ("404 Not Found \n" + getDate()).encode()
+
+def ServerError500():
+    return ("500 Server Error \n" + getDate()).encode()
+
+
+def getDate():
+    return "Date: " + str(date.today())
+
+def getContentType():
+    return "Content-Type: text/"
+
+def getContentLength():
+    return str(6)
+
 
 
 s = socket.socket()
