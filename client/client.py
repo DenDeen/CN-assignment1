b56c39419ed930503efb6bb345ae4c5f2cb347eb
DenDeen <mikkel.skovdal@student.ucll.be>
2022-04-19 16:39:56 +0200

    fak oef

--- client/client.py
+++ client/client.py
@@ -72,11 +72,10 @@ def getRequest(host, port):
             i = 1
             for img in soup.find_all("img"):
                 try:
-                    print(img)
                     img_uri = img['src']
                     if img_uri[0] != '/':
                         img_uri = '/'+img_uri
-                    s.sendall(('GET ' + img_uri + ' HTTP/1.1\r\nHost:%s\r\n\r\n' % host).encode())
+                    s.sendall(('GET ' + img_uri + ' HTTP/1.1\r\nHost:%s\r\n\r\n' % host.split("/",1)[0]).encode())
                     _, image_data =  recv_all(s)
                     # save image
                     with open('client/{}_image_{}.png'.format(getHost(host),i), 'wb') as image_file:
