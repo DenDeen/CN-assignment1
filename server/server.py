4ccd7c733d4edc5b0885b8cb6a17211a5044a09e
brianverbanck <brianverbanck@hotmail.com>
2022-04-21 16:34:45 +0200

    added docs

--- server/server.py
+++ server/server.py
@@ -18,44 +18,37 @@ def getRequest(connection, requestFile, headers):
         #Add server to file path    
         path =os.path.join("server",file) 
         file = open(path,'rb') 
-        #Check if "If-Modified-since" header is sended
+
         if("If-Modified-Since: " in headers): 
-            #Get the date
+            #Get the date and convert it
             modifiedSince = headers.split("If-Modified-Since: ",1)[1].split("\r\n",1)[0]
-            #Convert it to a datetime_object
             datetime_object = datetime.strptime(modifiedSince, '%a, %d %b %Y %X %Z')
             #See if datetime is after the date of the file last modification, if so return 304, otherwise return 200 and content
             if  datetime.timestamp(datetime_object) > os.path.getctime(path):
                 header = 'HTTP/1.1 304 NOT MODIFIED'
                 response = b""
             else:
-                #Read and close file
                 response = file.read()
                 file.close()
                 
-                #Build header
                 header = 'HTTP/1.1 200 OK\n'
                 ContentType = customLibrary.getContentType(path)
                 header += 'Content-Type: '+str(ContentType)+ '\n' + customLibrary.getDate() + " \n" + "Content-length: " + str(len(response))
 
         else:
-            #Read and close file
             response = file.read()
             file.close()
             
-             #Build header
             header = 'HTTP/1.1 200 OK\n'
             ContentType = customLibrary.getContentType(path)
             header += 'Content-Type: '+str(ContentType)+ '\n' + customLibrary.getDate() + " \n" + "Content-length: " + str(len(response))
 
-       
-    #Handle files not found and other exceptions by building a 404 response
     except Exception as e:
         print(e)
         header = 'HTTP/1.1 404 Not Found\n\n ' 
         response = '<html><body><center><h3>Error 404: File not found</h3></center></body></html>'.encode('utf-8')
         
-    #Make header and response ready to send trough to the client, and send it
+    #Returns response to client
     final_response = header.encode('utf-8')
     final_response += b'\r\n\r\n'
     final_response += response 
@@ -65,13 +58,18 @@ def getRequest(connection, requestFile, headers):
 def headRequest(connection, requestFile, headers):
     file = customLibrary.formatFile(requestFile)
     try:
+        #If path ends on nothing, try to get index.html
         if file[-1] == "/" or file == '':
             file += "index.html"
+        
+        #Add server to file path    
         path =os.path.join("server",file) 
         file = open(path,'rb') 
         if("If-Modified-Since: " in headers): 
+            #Get the date and convert it
             modifiedSince = headers.split("If-Modified-Since: ",1)[1].split("\r\n",1)[0]
             datetime_object = datetime.strptime(modifiedSince, '%a, %d %b %Y %X %Z')
+            #See if datetime is after the date of the file last modification, if so return 304, otherwise return 200 and content
             if  datetime.timestamp(datetime_object) > os.path.getctime(path):
                 header = 'HTTP/1.1 304 NOT MODIFIED'
             else:
@@ -94,20 +92,27 @@ def headRequest(connection, requestFile, headers):
         print(e)
         header = 'HTTP/1.1 404 Not Found\n\n'
         
+    #Returns response to client 
     connection.send(header.encode('utf-8'))
     
 #HTTP POST Request
 def postRequest(connection, requestFile, request):  
+    #format file and split data from request
     file = customLibrary.formatFile(requestFile)
     data = request.split("?data='",1)[1].split("'")[0]
     
+    #return Bad request if there isnt a file name at the end of the path
     if file == '/' or file == '':
         header = 'HTTP/1.1 400 Bad request\n\n'
     else:
         try:
+            #Only allow .txt files
             if(file.endswith(".txt")):
+                #Create paths to the new file
                 customLibrary.createPaths(file)
+                #Add server to the relative path to the file
                 path = os.path.join("server",file)
+                #See if file already excist, if so add a new line before appending
                 if os.path.exists(path):
                     file = open(path,'a')
                     file.write("\n")
@@ -122,27 +127,30 @@ def postRequest(connection, requestFile, request):
         except Exception as e:
             print(e)
             header = 'HTTP/1.1 500 Server error\n\n'
-    
+    #return response header
     final_response = header.encode('utf-8')
     connection.send(final_response)
 
-#P
+#HTTP Put Request
 def putRequest(connection, requestFile, request):
+    #format file and split data from request
     file = customLibrary.formatFile(requestFile)
     data = request.split("?data='",1)[1].split("'")[0]
     
+    #return Bad request if there isnt a file name at the end of the path
     if file == '/' or file == '':
-        print("hallo")
         header = 'HTTP/1.1 400 Bad request\n\n'
     else:
         try:
+            #Only allow .txt files
             if(file.endswith(".txt")):
+                #Create paths to the new file
                 customLibrary.createPaths(file)
+                #Add server to the relative path to the file
                 path =os.path.join("server",file) 
                 file = open(path,'w') 
                 file.write(data)
                 file.close()
-
                 header = 'HTTP/1.1 200 OK\n'
             else:
                 header = 'HTTP/1.1 400 Bad request\n\n'
@@ -150,7 +158,8 @@ def putRequest(connection, requestFile, request):
         except Exception as e:
             print(e)
             header = 'HTTP/1.1 500 Server error\n\n'
-    
+            
+    #Return response to client
     final_response = header.encode('utf-8')
     connection.send(final_response)
 
